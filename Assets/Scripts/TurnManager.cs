2da86e54178cd69bf71aca8d143f71b330790c31
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-07-16 19:59:38 +0200

    Turn Manager && AI Scipts d                                       i                                             o                                 c                      a                       n                             e

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class TurnManager : MonoBehaviour {
 
-    public PlayerActions player;
-    public Bracciante[] braccianti;
+    public GameObject[] charactersArray;
+    private int i = 0;
+    
 
-	// Use this for initialization
-	void Start () {
-        player.isMyTurn = true;
-        for(int i=0;i<braccianti.Length;i++)
-        {
-            braccianti[i].isMyTurn = false;
-        }
+
+    // Use this for initialization
+    void Start()
+    {
+        charactersArray[i].GetComponent<PlayerActions>().isMyTurn = true;
+        i++;
 	}
-	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+
+    }
+    /// <summary>
+    /// Cambio turno delle singole istnze dell'array 
+    /// </summary>
+    public void changeTurn()
+    {
+        if (charactersArray[i].gameObject.tag == "Bracciante")
+        {
+            charactersArray[i].GetComponent<Bracciante>().isMyTurn = true;
+        }
+        else if (charactersArray[i].gameObject.tag == "Prostituta")
+        {
+            charactersArray[i].GetComponent<RagazzaAmbiziosa>().isMyTurn = true;
+        }
+        else if (charactersArray[i].gameObject.tag == "Cowboy")
+        {
+            charactersArray[i].GetComponent<RagazzoMucca>().isMyTurn = true;
+        }
+        else if (charactersArray[i].gameObject.tag == "Player")
+        {
+            charactersArray[i].GetComponent<PlayerActions>().isMyTurn = true;
+        }
+        i++;
+     
+    }
 }
