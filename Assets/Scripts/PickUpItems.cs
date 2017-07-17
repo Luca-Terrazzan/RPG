704e1a06d1891e804aebaf953e8d0bd9fa5d991f
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-07-17 16:59:54 +0200

    Porcodio finalmente qualcosa che funziona mannaggia a cristo tanto non finiremo mai in tempo Gabriele dio suino. Come facciamo a finire per l'otto non lo so quindi vado a dormire che tanto non so far niente e dio madonna è sempre colpa del signore come facciamo a finire halp! Aggiunte collisioni con oggetti.

---
+++ Assets/Scripts/PickUpItems.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItems : MonoBehaviour {
+
+    private PlayerActions player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GetComponent<PlayerActions>();
+	}
+    // Metodo a caso per la collisione con gli oggetti, amazing.
+    private void OnTriggerEnter (Collider collision)
+    {
+       
+       if (collision.gameObject.tag == "Key")
+        {
+            player.hasKey = true;
+            Destroy(collision.gameObject);
+            Debug.Log("Hai preso la chiave.Amaze.");
+        }
+        else if (collision.gameObject.tag == "ExitDoor")
+         {
+            if (player.hasKey)
+            {
+                Debug.Log("Puoi uscire.Wow.");
+                // go to main scene and set hasKey to false
+            }
+            else
+            {
+                Debug.Log("Noob vai a prendere la chiave");
+            }
+         }
+
+       else if (collision.gameObject.tag == "OggettoRaccoglibileAcaso")
+        {
+            Debug.Log("Bravo hai preso un oggetto inutile.Amaze.");
+            // store object somewhere 
+        }
+    }
+    
+}
