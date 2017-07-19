1794bafa5860b3e229cd630c0bee78315eea606b
dbga_27 <dbga_27@dbga.local>
2017-07-19 14:06:58 +0200

    Added Camera movement, Prototipo Becchino, Fattoria Bracciante

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+	public GameObject Player;
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = new Vector3 (3f, -5f, -5f);
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = Player.transform.position + offset;
+	}
+}
