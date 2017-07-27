3259a68bd2bfde7b23200a5d408b677def48a7e2
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-07-27 15:29:52 +0200

    boh ho fatto cose che mi hanno detto di fare i designer non so bene cosa abbia modificato però mi sa che non funzionerà più niente

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public enum levels { Fattoria, Becchino, Prigione, Bordello, Corridoio, Saloon, Cimitero, duplè};
+    public levels GoToScene;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+            SceneManager.LoadScene(GoToScene.ToString());
+    }
+}
