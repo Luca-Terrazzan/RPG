9756a9cbc4b8fb6aad20ddc3072dd375b296fc30
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-08-01 10:43:13 +0200

    staggiamo

---
+++ Assets/Intro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+
+
+    //Do we, do we know, when we fly, when we, when we go, do we die.
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
