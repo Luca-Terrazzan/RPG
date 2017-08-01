bfd6b45ac7ed83ca6c0d7f75000100b71daa87d1
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-08-01 15:11:36 +0200

    ho messo quel porco dio di menu che mannaggia a dio senza scarpe e in croce è stato un dito nel culo. manano ancora i bottoni che possono interagire però mannaggia a quel porco del signore sto scrivendo a una velocità impressionante. comunque mannggia a dio siamo super indietro. oggi finisco di mettere le ultime cose dei menù e faccio i polish. Kneeling roses disappearing Into Moses' dry mouth. Breaking into Fort Knox, Stealing our  intentions.Hangars sitting dripped in oil, Crying, "Freedom!"

--- Assets/Intro.cs
+++ Assets/Intro.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Intro : MonoBehaviour {
 
+    //public bool stop = true;
 
-    //Do we, do we know, when we fly, when we, when we go, do we die.
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Image wakandaLogo, backgorundLogo;
+    private Button startGame, toDesktop;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        wakandaLogo = transform.GetChild(1).GetComponentInChildren<Image>();
+        backgorundLogo = transform.GetChild(0).GetComponent<Image>();
+        startGame = transform.GetChild(2).GetComponentInChildren<Button>();
+        toDesktop = transform.GetChild(3).GetComponentInChildren<Button>();
+
+    }
 	
+
+
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        
+        if (wakandaLogo.gameObject.transform.position.y <755 )
+        {
+            wakandaLogo.gameObject.transform.position = Vector3.Lerp(wakandaLogo.gameObject.transform.position,
+              new Vector3(wakandaLogo.transform.position.x, wakandaLogo.transform.position.y + 200F, wakandaLogo.transform.position.z)
+              , Time.deltaTime);
+        }
+        else
+        {
+            backgorundLogo.gameObject.SetActive(true);
+            startGame.gameObject.SetActive(true);
+            toDesktop.gameObject.SetActive(true);
+            
+        }
+        
+    }
 }
