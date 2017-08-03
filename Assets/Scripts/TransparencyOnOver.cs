6f91426a38a4eb63875d03d25a1eeb3beb14f977
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-08-03 19:32:45 +0200

    commit suoni

---
+++ Assets/Scripts/TransparencyOnOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparencyOnOver : MonoBehaviour {
+    private SpriteRenderer sprite;
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        sprite.color = new Color(1, 1, 1, 0.5f);
+    }
+
+    private void OnMouseExit()
+    {
+        sprite.color = new Color(1, 1, 1, 1);
+    }
+}
