36526dbe2ed5e73dd0626e730b635c2f271a0b07
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-08-07 21:42:09 +0200

    che bella questa commit porco il signore.

---
+++ Assets/Scripts/FeedbackFuocoFatuo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackFuocoFatuo : MonoBehaviour
+{
+
+    [SerializeField] SpriteRenderer feedBackFatuo, fuocoFatuo;
+	// Use this for initialization
+	void Start ()
+    {
+        feedBackFatuo = transform.GetChild(1).GetComponentInChildren<SpriteRenderer>();
+        fuocoFatuo = transform.GetChild(0).GetComponentInChildren<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        if (!fuocoFatuo.enabled)
+        { 
+            feedBackFatuo.enabled = true;
+        }
+    }
+    private void OnMouseExit()
+    {
+        feedBackFatuo.enabled = false;
+    }
+}
