71c7a530f9c4c6dd0bb3d614fb59e4640c18d242
Nikolas Lacquaniti <nikolas.lacquaniti@gmail.com>
2017-08-07 19:22:19 +0200

    boh io pusho

---
+++ Assets/Scripts/ResetStaticVariables.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetStaticVariables : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        FreeRoamingPos.i = 0;
+        FreeRoamingPos.staticFuochiFatui = 0;
+        FreeRoamingPos.j = 0;
+        FreeRoamingPos.triggerOnceEvent = true;
+
+    }
+	
+}
